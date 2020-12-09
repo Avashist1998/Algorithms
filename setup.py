182a5befc2659152a7c39396dee09d1f04b90afb
Avashist1998 <36799410+Avashist1998@users.noreply.github.com>
2020-12-08 23:59:40 -0600

    Changing Package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-        name="Algorithms",
+        name="Algorithms-abhay",
         version="0.0.1",
         author="Abhay Vashist",
         author_email="avashist98@tamu.edu",
